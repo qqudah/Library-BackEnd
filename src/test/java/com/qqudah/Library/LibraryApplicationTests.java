fbe8c8271ebe884e80c6716c5f11d14a080bb7eb
qqudah <mohannadqqudah@gmail.com>
2025-07-29 21:15:16 +0300

    init commit

---
+++ src/test/java/com/qqudah/Library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qqudah.Library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
